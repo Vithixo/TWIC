212627d0e5e28a5565b4cdcb798f23f30858d3ce
Samuel <samuel.bertin@reseau.eseo.fr>
2020-06-20 19:24:20 +0200

    correction codde smells

--- src/main/java/com/blo/VilleBLOImpl.java
+++ src/main/java/com/blo/VilleBLOImpl.java
@@ -1,6 +1,5 @@
 package com.blo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/main/java/com/config/JDBCConfiguration.java
+++ src/main/java/com/config/JDBCConfiguration.java
@@ -5,16 +5,20 @@ import java.sql.DriverManager;
 
 public class JDBCConfiguration {
 	
-	private final static String MDPBDD = "yhqyD4rJzto+K4";
+	private JDBCConfiguration() {
+		
+	}
+	
+	private static final String MDPBDD = "yhqyD4rJzto+K4";
+	
 	public static Connection getConnection() {
-		String BDD = "ville_france";
-	    String url = "jdbc:mysql://localhost:3306/" + BDD;
+		String bdd = "ville_france";
+	    String url = "jdbc:mysql://localhost:3306/" + bdd;
 	    String user = "somanager";
 	    
 	    Connection conn = null;
 	    // L'essaie de connexion à votre base de donées
 	    try {
-	        Class.forName("com.mysql.jdbc.Driver");
 	        conn = DriverManager.getConnection(url, user, MDPBDD);
 	    } catch (Exception e){
 	        e.printStackTrace();
